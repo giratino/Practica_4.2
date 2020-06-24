5bfeb71ff3e44e0aee9889f267e4549770d7c699
kcr201 <luis.cardenas193@tectijuana.edu.mx>
2020-06-24 15:36:06 -0700

    se hixo un comentario pendejo
    jajaja

--- src/Viborita/Ventana/Archivo.java
+++ src/Viborita/Ventana/Archivo.java
@@ -13,7 +13,7 @@ class Archivo
                     {
 
                         cadena = br.readLine();
-
+//hola este es unc ckmwiner
                         while(cadena!=null)
                             {
                                 contenido.add(cadena);
